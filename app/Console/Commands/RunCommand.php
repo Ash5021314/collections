eec713d4ed5581e857ef3e0c7c2db5886856569b
Ash5021314 <ashkapoghosyan@gmail.com>
2023-08-24 09:07:32 +0400

    asasas

--- app/Console/Commands/RunCommand.php
+++ app/Console/Commands/RunCommand.php
@@ -13,7 +13,7 @@ class RunCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:run-command';
+    protected $signature = 'run';
 
     /**
      * The console command description.
@@ -69,6 +69,7 @@ class RunCommand extends Command
         $nameCollection = collect(['Ivan', 'Boris', 'Kate', 'Kate']);
         $nameCollection2 = collect([20, 22, 18]);
         $anotherNameCollection = collect(['Ann', 'John']);
+        $anotherNameCollection2 = collect(['Ann'=>'boss', 'John'=>'developer']);
 
 //----------------avg--------------------
 
@@ -154,9 +155,34 @@ class RunCommand extends Command
 
 //----------------eachSpread--------------------
 
-$resoult = $arrayWorkerCollection->eachSpread(function ($name,$age){
-    dd($name,$age);
-});
+//$resoult = $arrayWorkerCollection->eachSpread(function ($name,$age){
+//    dd($name,$age);
+//});
+
+//----------------except--------------------
+
+//$resoult = $anotherNameCollection2->except(['John']);
+
+//----------------filter--------------------
+
+//$resoult = $anotherNumberCollections->filter(function ($value){
+//    return $value % 10 === 0;
+//});
+
+//----------------first--------------------
+
+//$resoult = $anotherNumberCollections->first();
+
+
+//----------------firstOrFail--------------------
+
+//$resoult = $anotherNumberCollections->firstOrFail(function ($value){
+//    return $value === 456;
+//});
+
+//----------------firstOrFail--------------------
+
+$resoult = $assocWorkerCollection->firstWhere('name','aaa');
 
 
         dd($resoult);
@@ -165,10 +191,10 @@ $resoult = $arrayWorkerCollection->eachSpread(function ($name,$age){
 
 //        all() - վերադարձնում է բոլոր էլէմենտները
 //        avg(),average - վերադարձնում է զանգվածի միջի թվերի միջինը
-//        chunk() - Collection-ը բաժանում է միքանի Collection-ի , բայց ամեն մեկի մեջ տեղադրում է մեր կողմից տրված արգումենտի քանակությամբ
-//        chunkWhile() - Collection-ը բաժանում է միքանի Collection-ի , բայց ամեն մեկի մեջ տեղադրում է մեր կողմից տրված արգումենտի քանակությամբ քանի դեռ իռ միջի պայմանը վերադարձնում է true
-//        collapse() - Եթե Զանգվածի մեջ ունենք էլի զանգվածներ, ապա այս մեթոդը միացնում է բոլորի վերադարձնելով է զանգված
-//        combine() - Մեթոդ է, որը թույլ է տալիս միավորել թ տարբեր զանգվածներ է ասոցատիվ զանգվածի,որտեղ առաջին զանգվածը դա key-նե իսկ երկրորդը value-ն, եթե երկուսնել ունեն նույն քանակությամբ էլէմենտներ
+//        chunk() - Մեթոդ է, որը բաժանում է միքանի Collection-ի , բայց ամեն մեկի մեջ տեղադրում է մեր կողմից տրված արգումենտի քանակությամբ
+//        chunkWhile() - Մեթոդ է, որը բաժանում է միքանի Collection-ի , բայց ամեն մեկի մեջ տեղադրում է մեր կողմից տրված արգումենտի քանակությամբ քանի դեռ իռ միջի պայմանը վերադարձնում է true
+//        collapse() - Եթե Զանգվածի մեջ ունենք էլի զանգվածներ, ապա այս մեթոդը միացնում է բոլորի վերադարցնելով է զանգված
+//        combine() - Մեթոդ է, որը թույլ է տալիս միավորել 2 տարբեր զանգվածներ է ասոցատիվ զանգվածի,որտեղ առաջին զանգվածը դա key-նե իսկ երկրորդը value-ն, եթե երկուսնել ունեն նույն քանակությամբ էլէմենտներ
 //        concat() - Մեթոդ է, որը միավորում է առաջին և երկրրորդ զանգվածի էլեմենտները, որպես value
 //        contains - Մեթոդ է, որը ստուգում է առկա է արդյոք նշված զանգվածի մեջ մեր կողմից փոխանցած ատրիբուտը, հաշվի առնելով մեծատառերն ու փոքրատառերը,կարող ենք փոխանցել callback function և կատարել տարատեսակ գործողություններ տարբեր պայմաններով հակառակն է անում doesntContain
 //        containsOneItem() - Մեթոդ է, որը ստուգում է առկա է արդյոք նշված զանգվածի մեջ մեկ էլէմենտ, եթե այո վերադարձնում է true, եթե ոչ վերադարձնում է false
@@ -182,6 +208,11 @@ $resoult = $arrayWorkerCollection->eachSpread(function ($name,$age){
 //        duplicates() - Մեթոդ է, որը վերադարձնում է ասոցատիվ զանգված, որի key-ը կրկնվող էլէմոենտի ինդեղն է , իսկ value -ն կրկնվող էլէմենտը
 //        duplicatesStrict() -  Նույն բաննէ անում ինչ duplicates() մեթոդը, բայց խիստ հավասարությունով
 //        eachSpread() -  Մեթոդ է, որը զանգվածի միջի էլեմենտնեը վերադարձնում է առանին առանձին
+//        except() -  Մեթոդ է, որը աշխատում է միյն ասոցատիվ զանգվածների հետ, վերադառձնում է նույն զանգվածը առանց փոխանցած արգումենտի,որը տվյալ զանգվածի քեյը պետք է լինի
+//        filter() -  Մեթոդ է, որը առանձնացնում է զանգվածի միյին են էլեմենտները, որոնք բավարարում են մեր կողմից տված պայմանին
+//        first() -  Մեթոդ է, որը վերադարձնում է զանգվածի առաջին էլեմենտը, կարող ենք փոխանցել callback function, որը կվերադարձնի պայմանին բավարարող էլեմենտներից առաջինը
+//        firstOrFail() -  Մեթոդ է, որը վերադարձնում է զանգվածի առաջին էլեմենտը, եթե չի գտնում՝ վերադարձնում է exception երրոր, որը կարող ենք օգտագործել try catch -ի մեջ
+//        firstWhere() -  Մեթոդ է, որը վերադարձնում է ասոցատիվ զանգվածի առաջին էլեմենտը, որը նշում ենք վօրպես արգումենտ key-ը և value-ն, եթե չի գտնում՝ վերադարձնում է null
 
     }
 }
